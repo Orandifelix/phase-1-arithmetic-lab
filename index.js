fe12cb65193e0f8a1c6be4189b007c4a3d7567ac
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-28 15:40:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+// num1 = 31
+// num2 = 2
+// multiply = (num1 * num2)
+// // console.log(multiply);
+
+// let random_number = Math.floor(Math.random() * 100) + 1;  
+// console.log(random_number);
+
+// // the remainder of dividing num3 by num4 to get 4
+// let num3 = 28;
+// let num4 = 6;
+// let mod = num3 % num4;
+// console.log(mod);  // Output: 4
+
+// //the highest number in a set
+// let numbers = [5, 20, 10, 15];
+// let max_number = Math.max(...numbers);
+// console.log(max_number);  // Output: 20
+let num1 = 31;
+let num2 = 2;
+let num3 = 20;
+let num4 = 4;
+let num5 = 24
+let num6 = 5
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num5 % num6;
+let max = Math.max(1, 5, 10, 20, 15);
